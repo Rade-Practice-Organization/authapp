4b2f87246e23cffcda3534a2deba93b49a8e5008
Rade <radoslav@kredium.rs>
2025-04-20 06:34:57 +0200

    Tenant roles

--- config/roles.php
+++ config/roles.php
@@ -5,11 +5,25 @@ return [
         'super-admin' => ['admin', 'user'],
         'admin' => ['user'],
         'user' => [],
+
+        'tenant_super-admin' => ['tenant_sales_manager'],
+        'tenant_sales_manager' => ['tenant_developer_admin'],
+        'tenant_developer_admin' => ['tenant_developer'],
+        'tenant_developer' => ['tenant_broker_admin', 'tenant_developer'],
+        'tenant_broker_admin' => ['tenant_broker_admin'],
+        'tenant_broker' => [],
     ],
 
     'abilities' => [
         'super-admin' => ['data:update', 'data:delete', 'data:view', 'data:create'],
         'admin' => ['data:update', 'data:view'],
         'user' => ['data:view'],
+
+        'tenant_super-admin' => ['tenant:admin'],
+        'tenant_sales_manager' => ['sales_manager:all'],
+        'tenant_developer_admin' => ['developer:create', 'developer:delete'],
+        'tenant_developer' => ['developer:view', 'developer:update'],
+        'tenant_broker_admin' => ['broker:create', 'broker:delete'],
+        'tenant_broker' => ['broker:view', 'broker:update'],
     ],
 ];
