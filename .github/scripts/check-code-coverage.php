18cf93a0adfa0bbe9b86fd14546a30ad24bccf26
Rade <radoslav@kredium.rs>
2025-05-18 12:46:17 +0200

    remove one yml file

--- .github/scripts/check-code-coverage.php
+++ .github/scripts/check-code-coverage.php
@@ -13,6 +13,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ *
  * This software consists of voluntary contributions made by many individuals
  * and is licensed under the MIT license.
  */
