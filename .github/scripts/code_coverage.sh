3e9aebe26df8be4a792751b39a1a5c25709afcef
Rade <radoslav@kredium.rs>
2025-05-18 12:26:17 +0200

    .ci.yml

--- .github/scripts/code_coverage.sh
+++ .github/scripts/code_coverage.sh
@@ -4,4 +4,4 @@ echo "==========================================================================
 echo "Coverage"
 echo "================================================================================"
 echo "Code coverage minimum: $PHPUNIT_CODE_COVERAGE_PERCENTAGE_MINIMUM%"
-php .github/script/check-code-coverage.php ./clover.xml "$PHPUNIT_CODE_COVERAGE_PERCENTAGE_MINIMUM"
+php .github/scripts/check-code-coverage.php ./clover.xml "$PHPUNIT_CODE_COVERAGE_PERCENTAGE_MINIMUM"
